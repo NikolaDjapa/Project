4f8ab6dc3892e37c3cfb69d460694f0481934860
NikolaDjapa <nikola.djapa@gmail.com>
2017-08-30 10:06:07 +0200

    a
    s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+<<<<<<< HEAD
 import {             //data import
   LoginDataComponent,
   
