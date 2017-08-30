cad664002d0b4665cdabaa6ba6c69a5675525c54
NikolaDjapa <nikola.djapa@gmail.com>
2017-08-30 10:09:52 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import {             //data import
   LoginDataComponent,
   
 }
 from "./data/data.all"
-=======
 import { LoginComponent} from './login/login-component/login.component'
->>>>>>> 411386fd25ee9dd650129e8b39b9cad7e027efc9
 
 const routes: Routes = [
   {
